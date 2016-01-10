a6112729468941074299b81f2e15689fd680b7fe
freas1990 <yinlong.lee@hotmail.com>
2016-01-10 06:57:53 -0800

    add rocksdb-3.9

--- tests/testrocksdb.cc
+++ tests/testrocksdb.cc
@@ -1,11 +1,13 @@
+#include <cstdio>
+#include <string>
 #include <iostream>
-#inlcude "rocksdb-1.5.7/db.h"
+#inlcude "rocksdb/db.h"
 
+using namespace rocksdb;
 
 int main() {
     rocksdb::DB* db;
     rocksdb::Options options;
     options.create_if_missing = true;
-    rocksdb::Status status =
-      rocksdb::DB::Open(options, "/tmp/testdb", &db);
+    rocksdb::Status status = rocksdb::DB::Open(options, "/tmp/testdb", &db);
 }
