0229a417634dbef9a3d5656cc7aeb8dd60d58d3a
freas1990 <yinlong.lee@hotmail.com>
2016-01-12 00:04:00 -0800

    update License file.

--- kernel/ds.cc
+++ kernel/ds.cc
@@ -1,3 +1,13 @@
+/*
+* Copyright 2015, Liyinlong.  All rights reserved.
+* https://github.com/Jacklli/ActionDB
+*
+* Use of this source code is governed by GPL v2 license
+* that can be found in the License file.
+*
+* Author: Liyinlong (yinlong.lee at hotmail.com)
+*/
+
 #include <stdlib.h>
 #include <pthread.h>
 #include <cstdio>
--- kernel/ds.h
+++ kernel/ds.h
@@ -1,4 +1,18 @@
+/*
+* Copyright 2015, Liyinlong.  All rights reserved.
+* https://github.com/Jacklli/ActionDB
+*
+* Use of this source code is governed by GPL v2 license
+* that can be found in the License file.
+*
+* Author: Liyinlong (yinlong.lee at hotmail.com)
+*/
+
+#ifndef __OBJECT_H__
+#define __OBJECT_H__
 
 int rocksSet(char *key, char *val);
 int rocksGet(char *key);
 void rocksShutdown();
+
+#endif
